1ff9024bf7b37479c4fb7e4ea0fd14c043c32eb1
xNikus <comlokoxd@gmail.com>
2022-04-26 14:44:43 +0200

    Icons + Update layout for daily forecast

---
+++ app/src/main/java/com/example/weatherapp/fragments/forecast_weather.java
@@ -0,0 +1,26 @@
+package com.example.weatherapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.weatherapp.R;
+
+
+public class forecast_weather extends Fragment {
+
+    public forecast_weather() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecast_weather, container, false);
+    }
+}
